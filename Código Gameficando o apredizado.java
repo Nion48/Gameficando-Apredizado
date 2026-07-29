4d96542b8031a9094cba7afd287e27db8b45f589
Nion48 <nionwallacy4@gmail.com>
2026-07-29 16:09:08 -0400

    Add files via upload

---
+++ Código Gameficando o apredizado.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GameEstudos {
+    public static void main(String[] args) {
+        List<String> historico = new ArrayList<>();
+        Scanner scanner = new scanner(System.in);
+        int opçao = 0;
+
+        //Minhas variáveis (personagem de game)
+        int xpAtual = 0;
+        int nivel = 1;
+        int xpParaUpar = 100;
+
+        while (opçao != 3) {
+            //Toda vez que o menu aparece, mostra o Status atual
+            System.out.printIn("\n=== STATUS ===");
+        }
+    }
+}
