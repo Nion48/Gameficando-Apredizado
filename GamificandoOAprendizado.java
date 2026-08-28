c0d8414334479f19ff081bec19d6a02a7f67e413
Nion48 <nionwallacy4@gmail.com>
2026-08-28 17:00:27 -0400

    Acrescimo ao código original

--- GamificandoOAprendizado.java
+++ GamificandoOAprendizado.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class GamificandoOAprendizado {
     public static void main(String[] args) {
-        List<String> historico = new ArrayList<>();
+        List<String> historicoExercicios = new ArrayList<>();
+        List<String> codigosAprendidos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int opçao = 0;
 
@@ -21,27 +22,33 @@ public class GamificandoOAprendizado {
             System.out.println("==========");
 
             System.out.println("1. Registrar Exercício Concluído (+XP)");
-            System.out.println("2. Ver Histórico do Treino");
-            System.out.println("3. Sair do Jogo");
+            System.out.println("2. Anotar Novo Código Aprendido (+XP)");
+            System.out.println("3. Ver Histórico Completo");
+            System.out.println("Sair do Jogo");
             System.out.println("Escolha uma acao: ");
             opçao = scanner.nextInt();
             scanner.nextLine(); //Limpa o buffer
 
             if (opçao == 1) {
-                System.out.println("\nQual tipo de exercicio você fez?");
+                System.out.println("\nQual tipo de exercicio ou estudos você fez?");
                 System.out.println("A) Leitura/Teoria (+10 XP)");
                 System.out.println("B) Prática no Coddy/Faculdade (+30 XP)");
-                System.out.println("Escolha (A ou B): ");
+                System.out.println("C) Exercícios Físicos");
+                System.out.println("Escolha (A, B ou C): ");
                 String tipo =  scanner.nextLine();
 
                 if (tipo.equalsIgnoreCase("A")) {
                     xpAtual = xpAtual + 10;
-                    historico.add("Estudou Teoria.");
+                    codigosAprendidos.add("Estudou Teoria.");
                     System.out.println("Boa! Ganhou 10 de XP.");
                 } else if (tipo.equalsIgnoreCase("B")) {
                     xpAtual = xpAtual + 30;
-                    historico.add("Praticou Código.");
+                    codigosAprendidos.add("Praticou Código.");
                     System.out.println("Excelente! Ganhou 30 de XP.");
+                } else if (tipo.equalsIgnoreCase("C")){
+                    xpAtual = xpAtual + 20;
+                    historicoExercicios.add("Praticou Exercício Físico.");
+                    System.out.println("Muito Bem! Você Está Mais Próximo da Sua Melhor Forma Física Ganhou 20 de XP.");
                 }
 
                 // Lógica de subir de nível (If em ação!)
@@ -53,8 +60,8 @@ public class GamificandoOAprendizado {
                 }
             } else if (opçao == 2) {
                 System.out.println("\n--- Histórico de Conquistas ---");
-                for (int i = 0; i < historico.size(); i++) {
-                    System.out.println("- " + historico.get(i));
+                for (int i = 0; i < historicoExercicios.size(); i++) {
+                    System.out.println("- " + historicoExercicios.get(i));
                 }
             }    
         }
